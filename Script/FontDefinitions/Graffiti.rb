ada21be124a01e7098226966d30badf20a6579ee
Austin Henriksen <austin.r.henriksen79@gmail.com>
2020-09-05 13:45:29 -0400

    We now load vector images, and set 20 to be flowa3.

--- Script/FontDefinitions/Graffiti.rb
+++ Script/FontDefinitions/Graffiti.rb
@@ -6,7 +6,7 @@ module DiceGen::Fonts
         def initialize()
             super(name: "graffiti", folder: "/Users/austin/DiceStuff/Resources/VectorFonts/MadiGraffitiFont",
                   padding: 0.1)
-
+            @glyphs["20"] = VECTOR_IMAGES.glyphs["flowa3"]
         end
 
     end
--- Script/Fonts.rb
+++ Script/Fonts.rb
@@ -1,6 +1,15 @@
 
 module DiceGen::Fonts
 
+    # Class for storing all the vector images as a font so that other fonts can access them.
+    # We do this before loading any of the other fonts, since any of them might reference these glyphs.
+    class VECTOR_IMAGES < RawFont
+        def initialize()
+            super(name: "", folder: "/Users/austin/DiceStuff/Resources/VectorImages")
+        end
+    end
+    puts "    Loaded VECTOR_IMAGES"
+
     # Import all the ruby files in the 'FontDefinitions' diretory; Each of these defines a single font.
     (Dir["#{__dir__}/FontDefinitions/*.rb"]).each() do |file|
         require file
