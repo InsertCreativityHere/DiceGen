73809b4df0115a6e914de7114d5b9f9df094414f
Austin Henriksen <austin.r.henriksen79@gmail.com>
2020-09-14 19:24:35 -0400

    Rotated and finished the TrigonalTrapezohedron.

--- Script/DiceDefinitions/Trapezohedrons/TrigonalTrapezohedron.rb
+++ Script/DiceDefinitions/Trapezohedrons/TrigonalTrapezohedron.rb
@@ -2,10 +2,9 @@
 module DiceGen::Dice
     # This class defines the mesh model for a trigonal trapezohedron (non-standard D6).
     class TrigonalTrapezohedron < Die
-        # This constant controls how much the vertexes of the trapezohedrons protrude from the midline. A value of 0
-        # means they don't protrude at all (it reduces this shape to a hexagon), and a value of 1 makes the height of
-        # the trapezohedrons (from the midline) equal to the horizontal axis of the trapezoids. Setting it to
-        # 'TODO' produces a standard trigonal trapezohedron.
+        # This constant controls how much the vertexes of the trapezohedron protrude from the midline. A value of 0
+        # means they don't protrude at all (it reduces this shape to a hexagon), and a value of 1 produces a standard
+        # trigonal trapezohedron.
         VERTEX_SCALE = 1.0
 
         # Lays out the geometry for the die in a new ComponentDefinition and adds it to the main DefinitionList.
@@ -14,25 +13,30 @@ module DiceGen::Dice
             definition = Util::MAIN_MODEL.definitions.add(self.class.name)
             mesh = definition.entities()
 
-            c0 = Math.sqrt(2.0) / 4.0
+            c0 = 0.0
+            c1 = 0.5
+            c2 = 1.0 / Math.sqrt( 3.0)
+            c3 = 1.0 / Math.sqrt(12.0)
+            c4 = 1.0 / Math.sqrt(24.0) * VERTEX_SCALE
+            c5 = Math.sqrt(3.0 / 8.0)  * VERTEX_SCALE
             # Define all the points that make up the vertices of the die.
-            v0 = Geom::Point3d::new( c0,  c0,  c0)
-            v1 = Geom::Point3d::new( c0,  c0, -c0)
-            v2 = Geom::Point3d::new( c0, -c0,  c0)
-            v3 = Geom::Point3d::new( c0, -c0, -c0)
-            v4 = Geom::Point3d::new(-c0,  c0,  c0)
-            v5 = Geom::Point3d::new(-c0,  c0, -c0)
-            v6 = Geom::Point3d::new(-c0, -c0,  c0)
-            v7 = Geom::Point3d::new(-c0, -c0, -c0)
+            v0 = Geom::Point3d::new( c0,  c0,  c5)
+            v1 = Geom::Point3d::new( c0, -c0, -c5)
+            v2 = Geom::Point3d::new( c0,  c2,  c4)
+            v3 = Geom::Point3d::new( c0, -c2, -c4)
+            v4 = Geom::Point3d::new( c1,  c3, -c4)
+            v5 = Geom::Point3d::new( c1, -c3,  c4)
+            v6 = Geom::Point3d::new(-c1,  c3, -c4)
+            v7 = Geom::Point3d::new(-c1, -c3,  c4)
 
             # Create the faces of the die by joining the vertices with edges. #TODO FIX THIS
             faces = Array::new(6)
-            faces[0] = mesh.add_face([v0, v1, v5, v4])
-            faces[1] = mesh.add_face([v0, v4, v6, v2])
-            faces[2] = mesh.add_face([v0, v2, v3, v1])
-            faces[3] = mesh.add_face([v7, v3, v2, v6])
-            faces[4] = mesh.add_face([v7, v6, v4, v5])
-            faces[5] = mesh.add_face([v7, v5, v1, v3])
+            faces[0] = mesh.add_face([v0, v2, v6, v7])
+            faces[1] = mesh.add_face([v0, v7, v3, v5])
+            faces[2] = mesh.add_face([v0, v5, v4, v2])
+            faces[3] = mesh.add_face([v1, v4, v5, v3])
+            faces[4] = mesh.add_face([v1, v3, v7, v6])
+            faces[5] = mesh.add_face([v1, v6, v2, v4])
 
             #TODO MAKE THE SCALES!
             super(definition: definition, faces: faces)
