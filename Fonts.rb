81215b6f406a579c0bde902cd87cfb7527e4a3fd
Austin Henriksen <austin.r.henriksen79@gmail.com>
2020-12-15 17:44:46 -0500

    Overhauled font loading and fonts in general.
    Now instead of being instances of base classes in the Fonts module, each font family has it's own class in 'Definitions'.
    All the fonts for each font family are declared as constants inside the font family's class.
    Same way we handle die model variants being defined in their model's class.
    This avoids polluting the Fonts namespace (and even Definitions) with loads of internal/unrelated constants.

--- Fonts.rb
+++ Fonts.rb
@@ -1,5 +1,7 @@
 
-module DiceGen::Fonts
+module DiceGen
+module Fonts
+module Definitions
 
     puts "===== Loading Images ====="
     # Create a 'font' for loading in and storing all the vector images, so that other fonts can access them.
@@ -8,16 +10,25 @@ module DiceGen::Fonts
     # Create a constant that references the actual images, for convenience's sake.
     VECTOR_IMAGES = VECTOR_IMAGES_FONT.glyphs
 
-    puts "===== Loading Fonts ====="
     # Define a constant that stores the path to the root font directory, again for convenience's sake.
     FONT_DIR = "#{DiceGen::Util::RESOURCE_DIR}/VectorFonts"
 
     # Import all the ruby files in the 'FontDefinitions' directory; Each of these defines a font family.
-    load_message = ""
+    puts "===== Loading Glyphs ====="
     (Dir["#{__dir__}/FontDefinitions/**/*.rb"]).each() do |file|
         require file
-        load_message += "    Loaded #{File.basename(file, ".rb")}\n"
     end
-    puts load_message
 
+    # List all the fonts that were imported.
+    puts "===== Loading Fonts ====="
+    ((Definitions.constants().map{|c| Definitions.const_get(c)}).select{|c| c.instance_of? Class}).each() do |font|
+        family_name = font.name.split('::').last()
+        variants = font.constants().select{|c| font.const_get(c).is_a? Font}
+        variants = "#{variants}".gsub(":", "")
+
+        puts "    Loaded #{family_name}#{' ' * (20 - family_name.length())}#{variants}"
+    end
+
+end
+end
 end
