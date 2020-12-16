d36daa73ba2f9eff475cf87fd3c26631c1d74eca
Austin Henriksen <austin.r.henriksen79@gmail.com>
2020-12-16 10:54:32 -0500

    Swapped the order of Dice and Font stuff in DiceGen (it looks nicer)

--- DiceGen.rb
+++ DiceGen.rb
@@ -3,14 +3,14 @@ module DiceGen
 
     # Import the utility code first, since everything else uses it.
     require_relative "Util.rb"
-    # Import the core code for font definitions.
-    require_relative "Fonts.rb"
     # Import the core code for die definitions.
     require_relative "Dice.rb"
-    # Import all the fonts that we've created so far.
-    require_relative "FontLoader.rb"
+    # Import the core code for font definitions.
+    require_relative "Fonts.rb"
     # Import all the die model that we've created so far.
     require_relative "DieLoader.rb"
+    # Import all the fonts that we've created so far.
+    require_relative "FontLoader.rb"
 
     # Helper method that just forwards to the 'create_instance' method of the specified die model.
     def create_die(model:, font: nil, type: nil, group: nil, scale: 1.0, depth: 0.0, die_size: nil, font_size: nil, glyph_mapping: nil, transform: IDENTITY)
@@ -29,7 +29,7 @@ end
 
 # These lines make life easier, by making it so we don't have to explicitly state module names when typing into IRB.
 include DiceGen
-include DiceGen::Fonts
-include DiceGen::Fonts::Definitions
 include DiceGen::Dice
+include DiceGen::Fonts
 include DiceGen::Dice::Definitions
+include DiceGen::Fonts::Definitions
